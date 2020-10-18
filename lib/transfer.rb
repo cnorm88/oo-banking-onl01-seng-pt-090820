e400fa059ce0b4d685048203fb8c3c5c2a30f066
Crystal Norman <cnorm88@gmail.com>
2020-10-18 00:51:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,7 +7,11 @@ class Transfer
     @sender = sender
     @receiver = receiver
     @status = "pending"
+<<<<<<< HEAD
     @amount = amount
+=======
+    @amount = 50
+>>>>>>> 89cca73a4b50f3139b366186c63bbc249f70ce51
   end
   
   def sender
@@ -23,6 +27,7 @@ class Transfer
   end
 
   def valid?
+<<<<<<< HEAD
     true  if self.receiver.valid? && self.sender.valid?
 
   end
@@ -45,4 +50,30 @@ class Transfer
       @status = "reversed"
     end
   end
+=======
+    true if self.receiver.valid? && self.sender.valid?
+  end
+  
+  def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    # elsif @status == "complete"
+    #   puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
+  end
+
+  # def reverse_transfer
+  #   if @status == "complete"
+  #     @sender.balance += @amount
+  #     @receiver.balance -= @amount
+  #     @status = "reversed"
+  #   end
+  # end
+>>>>>>> 89cca73a4b50f3139b366186c63bbc249f70ce51
 end
